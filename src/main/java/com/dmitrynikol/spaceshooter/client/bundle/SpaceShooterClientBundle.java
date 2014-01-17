bcefb22dfe7831eb625ff51a8c2a1439139ed922
Peter Donald <peter@realityforge.org>
2014-01-18 10:03:21 +1100

    Move static resources to the correct directory

--- src/main/java/com/dmitrynikol/spaceshooter/client/bundle/SpaceShooterClientBundle.java
+++ src/main/java/com/dmitrynikol/spaceshooter/client/bundle/SpaceShooterClientBundle.java
@@ -7,67 +7,69 @@ import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.ImageResource;
 
 /**
- * 
  * @author Dmitry Nikolaenko
- *
  */
-public interface SpaceShooterClientBundle extends ClientBundle {
-	
-	public interface StyleResources extends CssResource {
-		@ClassName("start")
-		String start();
-	}
-	
-	static final SpaceShooterClientBundle INSTANCE = GWT.create(SpaceShooterClientBundle.class);
-	
-	@Source("resources/css/space-shooter-game.css")
-	StyleResources css();
-	
-	@Source("resources/images/spaceship.png")
-	ImageResource spaceship();
-	
-	@Source("resources/images/asteroid1.png")
-	ImageResource asteroid1();
-	
-	@Source("resources/images/asteroid2.png")
-	ImageResource asteroid2();
-	
-	@Source("resources/images/asteroid3.png")
-	ImageResource asteroid3();
-	
-	@Source("resources/images/galaxy.jpg")
-	ImageResource galaxy();
-	
-	@Source("resources/images/spacesky.jpg")
-	ImageResource spacesky();
-	
-	@Source("resources/images/freeze.png")
-	ImageResource freeze();
-	
-	@Source("resources/images/reflex_boost.png")
-	ImageResource reflex_boost();
-	
-	@Source("resources/images/shield.png")
-	ImageResource shield();
-	
-	@Source("resources/images/x2_exp.png")
-	ImageResource x2_exp();
-	
-	@Source("resources/images/medikit.png")
-	ImageResource medikit();
+public interface SpaceShooterClientBundle
+  extends ClientBundle
+{
 
-	@Source("resources/sound/bump.wav")
-	public DataResource bumpSound();
-	
-	@Source("resources/sound/shield.wav")
-	public DataResource shieldSound();
-	
-	@Source("resources/sound/frezee.wav") 
-	public DataResource frezeeSound();
-	
-	@Source("resources/sound/the_end.ogg")
-	public DataResource theEndSound();
-	
-	@Source("resources/sound/game_theme.ogg")
-	public DataResource gameThemeSound();
+  public interface StyleResources
+    extends CssResource
+  {
+    @ClassName( "start" )
+    String start();
+  }
+
+  static final SpaceShooterClientBundle INSTANCE = GWT.create( SpaceShooterClientBundle.class );
+
+  @Source( "css/space-shooter-game.css" )
+  StyleResources css();
+
+  @Source( "images/spaceship.png" )
+  ImageResource spaceship();
+
+  @Source( "images/asteroid1.png" )
+  ImageResource asteroid1();
+
+  @Source( "images/asteroid2.png" )
+  ImageResource asteroid2();
+
+  @Source( "images/asteroid3.png" )
+  ImageResource asteroid3();
+
+  @Source( "images/galaxy.jpg" )
+  ImageResource galaxy();
+
+  @Source( "images/spacesky.jpg" )
+  ImageResource spacesky();
+
+  @Source( "images/freeze.png" )
+  ImageResource freeze();
+
+  @Source( "images/reflex_boost.png" )
+  ImageResource reflex_boost();
+
+  @Source( "images/shield.png" )
+  ImageResource shield();
+
+  @Source( "images/x2_exp.png" )
+  ImageResource x2_exp();
+
+  @Source( "images/medikit.png" )
+  ImageResource medikit();
+
+  @Source( "sound/bump.wav" )
+  public DataResource bumpSound();
+
+  @Source( "sound/shield.wav" )
+  public DataResource shieldSound();
+
+  @Source( "sound/frezee.wav" )
+  public DataResource frezeeSound();
+
+  @Source( "sound/the_end.ogg" )
+  public DataResource theEndSound();
+
+  @Source( "sound/game_theme.ogg" )
+  public DataResource gameThemeSound();
 }
