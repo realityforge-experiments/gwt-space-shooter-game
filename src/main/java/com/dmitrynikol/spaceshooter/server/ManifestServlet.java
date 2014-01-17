b6543ff54255f5ef9aa45c7a1e55d588e2e3ae7f
Peter Donald <peter@realityforge.org>
2014-01-18 10:19:43 +1100

    Make sure the manifest servlet is registered

---
+++ src/main/java/com/dmitrynikol/spaceshooter/server/ManifestServlet.java
@@ -0,0 +1,15 @@
+package com.dmitrynikol.spaceshooter.server;
+
+import javax.servlet.annotation.WebServlet;
+import org.realityforge.gwt.appcache.server.AbstractManifestServlet;
+import org.realityforge.gwt.appcache.server.propertyprovider.UserAgentPropertyProvider;
+
+@WebServlet(urlPatterns = { "/spaceshooter.appcache" })
+public class ManifestServlet
+  extends AbstractManifestServlet
+{
+  public ManifestServlet()
+  {
+    addPropertyProvider( new UserAgentPropertyProvider() );
+  }
+}
